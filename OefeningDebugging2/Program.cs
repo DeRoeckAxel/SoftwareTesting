7fc33c911ae356b13cccec0227a2de0b1a623621
Axel De Roeck <axel.deroeck@student.ap.be>
2020-10-06 13:54:05 +0200

    backup 06/10/20

---
+++ OefeningDebugging2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OefeningDebugging2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letters = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+            for (int i = 0; i <= letters.Length-1; i++)
+            {            
+                Console.WriteLine(letters[i].ToLower());
+            }
+        }
+    }
+}
